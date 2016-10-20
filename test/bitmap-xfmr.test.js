8f2376743c268c69df7e1b261fda0eed1e1aa430
Mark Greenwood <mrkjgreenwood@gmail.com>
2016-10-20 10:54:07 -0700

    Added very basic first test

--- test/bitmap-xfmr.test.js
+++ test/bitmap-xfmr.test.js
@@ -2,6 +2,9 @@ const assert = require('chai').assert;
 
 describe('buffer read', function() {
   it ('reads buffer from file', function(done) {
-    
+    fs.readFile('non-palette-bitmap.bmp', (err, buffer) => {
+      assert(buffer instanceof Buffer);
+      done(err);
+    });
   }
 });
