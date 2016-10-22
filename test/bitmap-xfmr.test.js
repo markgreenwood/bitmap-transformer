5a072feadb487cb36ad54fd2e4592e58ce42f3db
Mark Greenwood <mrkjgreenwood@gmail.com>
2016-10-22 08:06:27 -0700

    Added some 'golden chickens' for test; started to add code to read image data (pixels) as a linear array

--- test/bitmap-xfmr.test.js
+++ test/bitmap-xfmr.test.js
@@ -19,6 +19,15 @@ describe('buffer read', function() {
 
 describe('Bitmap constructor', function() {
 
+  it('constructs an empty Bitmap object and throws error if you try to use it before reading a file', function() {
+    // let testBmp = new Bitmap();
+    // expect(testBmp.transform('redder', 2)).to.throw(Error);
+  });
+
+});
+
+describe('Bitmap constructor with palette bitmap', function() {
+
   let myBmp;
   before(function() {
     // TODO: convert to async constructor
@@ -39,7 +48,29 @@ describe('Bitmap constructor', function() {
 
 });
 
-describe('invertColors transform', function() {
+describe('Bitmap constructor with non-palette bitmap', function() {
+
+  let myBmp;
+  before(function() {
+    // TODO: convert to async constructor
+    myBmp = new Bitmap('non-palette-bitmap.bmp');
+  });
+
+  it('properly extracts bmp file header on non-palette bitmap file', function() {
+    let expected = JSON.parse(fs.readFileSync('test/non-palette-bitmap-bmp-file-header.json'));
+    let actual = myBmp.bmp_file_header;
+    assert.deepEqual(actual, expected);
+  });
+
+  it('properly extracts DIB header on non-palette bitmap file', function() {
+    let expected = JSON.parse(fs.readFileSync('test/non-palette-bitmap-dib-header.json'));
+    let actual = myBmp.dib_header;
+    assert.deepEqual(actual, expected);
+  });
+
+});
+
+describe('palette invertColors transform', function() {
 
   let myBmp;
   before(function() {
@@ -60,6 +91,24 @@ describe('invertColors transform', function() {
       };
     });
     assert.deepEqual(actual, expected);
+  });
+
+});
+
+describe('palette redder x3 transform', function() {
+
+  let myBmp;
+  before(function() {
+    // TODO: convert to async constructor
+    myBmp = new Bitmap('palette-bitmap.bmp');
+  });
+
+  it('matches buffer from golden chicken test/palette-redder3.bmp', function() {
+    let ref_buffer = fs.readFileSync('test/palette-redder3.bmp');
+    myBmp.transform('redder', 3);
+    myBmp.updateBufferColorTable();
+    let actual = myBmp.buffer;
+    assert.deepEqual(actual, ref_buffer);
   });
 
 });
