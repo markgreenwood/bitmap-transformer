bff93d2ae33e297f0cbb43e5bcd9faa45eeae0ee
Mark Greenwood <mrkjgreenwood@gmail.com>
2016-10-21 17:06:37 -0700

    Some cleanup, plus writing new tests to compare read-from-file headers to 'gold standard' headers from those same files stored as JSON

--- test/bitmap-xfmr.test.js
+++ test/bitmap-xfmr.test.js
@@ -1,15 +1,15 @@
 const assert = require('chai').assert;
-const expect = require('chai').expect;
+// const expect = require('chai').expect;
 const fs = require('fs');
 const Bitmap = require('../lib/bitmap-xfmr');
 
-const filetypes = ['BM', 'BA', 'CI', 'CP', 'IC', 'PT'];
+// const filetypes = ['BM', 'BA', 'CI', 'CP', 'IC', 'PT'];
 
 describe('buffer read', function() {
 
   it ('reads buffer from file', function(done) {
     fs.readFile('palette-bitmap.bmp', (err, buffer) => {
-      console.log(buffer);
+      // console.log(buffer);
       assert(buffer instanceof Buffer);
       done(err);
     });
@@ -17,19 +17,23 @@ describe('buffer read', function() {
 
 });
 
-describe('constructor', function() {
+describe('Bitmap constructor', function() {
 
   let myBmp;
   before(function() {
     myBmp = new Bitmap('palette-bitmap.bmp');
   });
 
-  it('extracts file type from the bmp file header', function() {
-    expect(myBmp.bmp_file_header.ftype).to.be.oneOf(filetypes);
+  it('properly extracts bmp file header', function() {
+    let expected = JSON.parse(fs.readFileSync('test/palette-bitmap-bmp-file-header.json'));
+    let actual = myBmp.bmp_file_header;
+    assert.deepEqual(actual, expected);
   });
 
-  it('extracts correct image size', function() {
-    assert.equal(myBmp.dib_header.img_size, (myBmp.bmp_file_header.fsize - myBmp.bmp_file_header.img_data_offs));
+  it('properly extracts DIB header', function() {
+    let expected = JSON.parse(fs.readFileSync('test/palette-bitmap-dib-header.json'));
+    let actual = myBmp.dib_header;
+    assert.deepEqual(actual, expected);
   });
 
 });
