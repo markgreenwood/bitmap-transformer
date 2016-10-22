665ccb8116e0403ede854875838b723764fb6ff4
Mark Greenwood <mrkjgreenwood@gmail.com>
2016-10-21 17:40:49 -0700

    Changed function names, added getColorTable method, wrote initial test code for invertColors() transform

--- test/bitmap-xfmr.test.js
+++ test/bitmap-xfmr.test.js
@@ -24,15 +24,40 @@ describe('Bitmap constructor', function() {
     myBmp = new Bitmap('palette-bitmap.bmp');
   });
 
-  it('properly extracts bmp file header', function() {
+  it('properly extracts bmp file header on palette bitmap file', function() {
     let expected = JSON.parse(fs.readFileSync('test/palette-bitmap-bmp-file-header.json'));
     let actual = myBmp.bmp_file_header;
     assert.deepEqual(actual, expected);
   });
 
-  it('properly extracts DIB header', function() {
+  it('properly extracts DIB header on palette bitmap file', function() {
     let expected = JSON.parse(fs.readFileSync('test/palette-bitmap-dib-header.json'));
     let actual = myBmp.dib_header;
+    assert.deepEqual(actual, expected);
+  });
+
+});
+
+describe('invertColors transform', function() {
+
+  let myBmp;
+  before(function() {
+    myBmp = new Bitmap('palette-bitmap.bmp');
+  });
+
+  it('inverts the color table', function() {
+    let initColorTbl = myBmp.getColorTable();
+    myBmp.invertColors();
+    let actual = myBmp.getColorTable();
+    let expected = initColorTbl.forEach(function(color) {
+      return {
+        red: 255 - color.red,
+        blue: 255 - color.blue,
+        green: 255 - color.green
+      };
+    });
+    console.log('actual: ', actual);
+    console.log('expected: ', expected);
     assert.deepEqual(actual, expected);
   });
 
